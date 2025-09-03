9a6f6c1ea8b68e33b752fd3d2e202e3188ef1b7e
현성 <156384723+Janghyunsung2@users.noreply.github.com>
2025-09-03 17:57:58 +0900

    [level 2] Title: 가장 큰 수, Time: 160.43 ms, Memory: 142 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.stream.*;;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String result = Arrays.stream(numbers)
+            .mapToObj(String::valueOf)
+            .sorted((a, b) -> (b + a).compareTo(a + b))
+            .collect(Collectors.joining());
+        
+
+        if (result.charAt(0) == '0') {
+            return "0";
+        }
+        return result;
+    }
+}
